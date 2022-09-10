de138c847bfec9d7d82987b558d3cec601d735cc
ReiAkio <tanaka1557@gmail.com>
2022-09-10 09:10:14 -0300

    Terminado apostila 6

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -7,21 +7,31 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const eventos = []
+
+
 app.post('/eventos', (req, res) => {
     const evento = req.body;
-    
-    //envia o evento para o microsserviço de lembretes
-    axios.post('http://localhost:4000/eventos', evento);
-    //envia o evento para o microsserviço de obervações
-    axios.post('http://localhost:5000/eventos', evento);
-    //envia o evento para o microsserviço de consulta
-    axios.post("http://localhost:6000/eventos", evento);
-    //envia o evento para o microsservico de classificacao
-    axios.post("http://localhost:7000/eventos", evento);
-    res.status(200).send({ msg: "ok"});
+    eventos.push(evento)
+
+    try{
+        //envia o evento para o microsserviço de lembretes
+        axios.post('http://localhost:4000/eventos', evento).catch(error => console.log("Error "+ error));
+        //envia o evento para o microsserviço de obervações
+        axios.post('http://localhost:5000/eventos', evento).catch(error => console.log("Error "+ error));
+        //envia o evento para o microsserviço de consulta
+        axios.post("http://localhost:6000/eventos", evento).catch(error => console.log("Error "+ error));
+        //envia o evento para o microsservico de classificacao
+        axios.post("http://localhost:7000/eventos", evento).catch(error => console.log("Error "+ error));
+    }
+    catch(err){};
+        res.status(200).send({ msg: "ok"});
+        
 });
 
-
+app.get('/eventos', (req, res) => {
+    res.send(eventos)
+    })
 
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000');
--- consulta/index.js
+++ consulta/index.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 
 const baseConsulta = {};
+const axios = require("axios");
 
 const funcoes = {
     LembreteCriado: (lembrete) => {
@@ -38,4 +39,15 @@ app.post("/eventos", (req, res) => {
     res.status(200).send({ msg: "ok" });
 });
 
-app.listen(6000, () => console.log("Consultas. Porta 6000"));
+app.listen(6000, async () => {
+    console.log("Consultas. Porta 6000");
+    const resp = await
+    axios.get("http://localhost:10000/eventos");
+    //axios entrega os dados na propriedade data
+    resp.data.forEach((valor, indice, colecao) => {
+    try {
+    funcoes[valor.tipo](valor.dados);
+    } catch (err) {}
+    });
+    });
+    
